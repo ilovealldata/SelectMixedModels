3ae90f88c5d6d1f9eea2f83bb714dc3a2b108ba3
Yonghee Lee <ilovealldata@gmail.com>
2017-07-20 02:34:44 +0900

    2nd exp

--- src/conAIC.jl
+++ src/conAIC.jl
@@ -1,3 +1,4 @@
+#function definition
 function conAIC(m::LinearMixedModel)
 
     rho = traceHat(m)
@@ -8,8 +9,7 @@ function conAIC(m::LinearMixedModel)
     cr=zeros(Float64,2)
     cr[1]=nn*(nn-pp-1.0)/((nn-pp)*(nn-pp-2.0))
     cr[2]=nn*(pp+1.0)/((nn-pp)*(nn-pp-2.0))
-    
+
     val=-2.0*llk+2.0*(cr[1]*(1+rho)+cr[2])
     IC(llk, rho, "VB", cr, val )
 end
-
